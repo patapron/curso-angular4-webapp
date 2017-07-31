cdd36641c32df57eed918a8494178b13cc81b02c
Miguel Ángel <pronick@gmail.com>
2017-07-31 19:30:27 +0200

    ngx-translation included

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Productos Angular 4';
+
+      constructor(private translate: TranslateService) {
+        translate.addLangs(['en', 'fr', 'ur', 'es']);
+        translate.setDefaultLang('en');
+        const browserLang = translate.getBrowserLang();
+        translate.use(browserLang.match(/en|fr|ur|es/) ? browserLang : 'en');
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,32 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { Http, HttpModule } from '@angular/http';
+
+
+export function HttpLoaderFactory(http: Http) {
+  // for development
+  // return new TranslateHttpLoader(http, '/start-angular/SB-Admin-BS4-Angular-4/master/dist/assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
+}
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule,
+    HttpModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [Http]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
